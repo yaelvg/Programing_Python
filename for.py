c03069e525ea9a1d4aff134c0cc3a0bf570b54b5
Alejandro Valdez <av192851@gmail.com>
2023-09-08 15:56:08 -0600

    Ejercicios de clase

---
+++ for.py
@@ -0,0 +1,20 @@
+'''Una caracteristica sobre for es que se puede usar un else al final
+for i in range(1,11):
+    if i ==3:
+        continue
+    print(i)
+else:
+    print('El ciclo for a terminado')
+x=0
+while x<10:
+    pass
+    '''
+#Funciones en python
+def tablas(n: int):
+    '''Genera una tabla
+    Args:
+    n(int): la tabla que se desea
+    '''
+    pass
+numero = int(input('Numero de la tabla:'))
+tablas(numero)
