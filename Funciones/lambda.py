f19d735cea96a09f6152a298e18c1b68a8f8c8eb
Alejandro Valdez <av192851@gmail.com>
2023-09-12 15:44:27 -0600

    Funciones

---
+++ Funciones/lambda.py
@@ -0,0 +1,8 @@
+#Funcion lambda
+import funciones as x
+fun=lambda a , b : a*b
+x= fun(2,3)
+print(x)
+'''Importar archivos'''
+y= x.suma(23,32)
+print(y)
---
+++ Numpy.py
@@ -0,0 +1,6 @@
+import Numpy
+'''Codigo con numpy'''
+#Funcion lambda
+fun=lambda a , b : a*b
+x= fun(2,3)
+print(x)
