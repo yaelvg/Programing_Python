61b74074d0c8dc11d59f826a2aa13f3aa89eb173
Alejandro Valdez <av192851@gmail.com>
2023-09-23 11:35:44 -0600

    cambios

--- Numpy.py
+++
@@ -1,6 +0,0 @@
-import Numpy
-'''Codigo con numpy'''
-#Funcion lambda
-fun=lambda a , b : a*b
-x= fun(2,3)
-print(x)
--- eje.py
+++
@@ -1,6 +0,0 @@
-# Completa el ejercicio
-numero=1
-while numero % 5 != 0:
-    numero = int(input('Introducir un numero: '))
-
-print('tu numero es ', numero)
