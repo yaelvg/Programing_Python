ffef62f5968d92fe3d1b64b4afe89f4e98065be4
Alejandro Valdez <av192851@gmail.com>
2023-09-23 21:21:39 -0600

    Practica 2

--- Practicas/Practica2_1.py
+++ Practicas/Practica2_1.py
@@ -7,45 +7,48 @@ Fecha: 26/09/2023
 '''
 class Persona:
     
-    #Constructor
     def __init__(self, nombre : str, edad : int, estura : float, numero : int) -> None:
-        self._nombre = nombre
-        self._edad = edad
-        self._estatura = estura
-        self._numero = numero
-    #Encapsulamiento
+        #Atributos privados
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__estatura = estura
+        self.__numero = numero
+        
+    #Encapsulamiento setters y getters
     @property
     def nombre(self):
-        return self._nombre
+        return self.__nombre
     @nombre.setter
     def nombre(self, nombre: str):
-        self._nombre = nombre
+        self.__nombre = nombre
     
     @property
     def edad(self):
-        return self._edad
+        return self.__edad
     @edad.setter
     def edad(self, edad: int):
-        self._edad = edad
+        self.__edad = edad
     
     @property
     def estatura(self):
-        return self._estatura
+        return self.__estatura
     @estatura.setter
     def estatura(self, estaura: float):
-        self._estatura = estaura
+        self.__estatura = estaura
     
     @property
     def numero(self):
-        return self._numero
+        return self.__numero
     @numero.setter
     def numero(self, numero: str):
-        self._numero = numero
+        self.__numero = numero
         
     def __str__(self) -> str:
-        return f'¡Hola! , Mi nombre es {self._nombre}, tengo {self._edad} años , nací en {2023-self._edad}.\n mido {self._estatura} metros y mi número de contacto es {self._numero} ¡Saludos!'
+        return f'¡Hola! , Mi nombre es {self.__nombre}, tengo {self.__edad} años , nací en {2023-self.__edad}.\n mido {self.__estatura} metros y mi número de contacto es {self.__numero} ¡Saludos!'
 
 def llenado():
+    """Guarda informacion proporcionada por el usuario
+    """
     for i in range(3):
         
         print(f'\n\tPersona {i+1}:\n')
@@ -54,16 +57,21 @@ def llenado():
         estatura_=float(input('Estatura en metros:'))
         numero_=int(input('Numero:'))
         
+        #Se crea una instacia de tipo persona para agregarla a una lista
         persona=Persona(nombre_,edad_,estatura_,numero_)
         personas.append(persona)
 
 def impresion():
+    """Impresion de datos
+    """
+    #Recorre las instancias de la lista de tipo Persona
     for i in range(3):
         print(f'\nPersona {i+1}:\n')
         print(personas[i])
 
 '''Funcion principal'''
 
+#Lista que contien instacias de tipo Persona
 personas = list()
 llenado()
 impresion()    
--- Practicas/Practica2_2.py
+++ Practicas/Practica2_2.py
@@ -1,12 +1,19 @@
+'''Practica numero 2 haciendo uso de clases y metodos - POO.
 
+Nombre: Yael Alejandro Valdez Gonzalez
+Boleta: 2022640608
+Practica 2
+Fecha: 26/09/2023
+'''
 import os
 
 class Profesor():
     
     def __init__(self, nombre_profesor : str, n_empleado : str) -> None:
+        #Atributos privados de la clase
         self.__nombre_profesor = nombre_profesor
         self.__n_empleado = n_empleado
-
+    #Encapsulamiento setters y getters
     @property
     def nombre_profesor(self):
         return self.__nombre_profesor
@@ -26,6 +33,7 @@ class Alumno():
     
     def __init__(self, nombre : str, apellidop : str, apellidom: str, nboleta: int, dia: int,
                  mes: int, año: int, carrera: str, grupo: str, correo: str) -> None:
+        #Atributos privados de la clase
         self.__nombre = nombre
         self.__apellidop =apellidop
         self.__apellidom =apellidom
@@ -108,23 +116,41 @@ class Alumno():
     def correo(self,correo):
         self.__correo = correo    
 class Lista():
-    
+    """Esta clase tiene variables 'atributos' de tipo Profesor y Alumno
+    """
     def __init__(self) -> None:
+        #Variable auxilar 
         self.__profesor= None
-        self.__lista = []
+        #Lista que contendra todos los alumnos de la clase alumnos
+        self.__lista = [] 
     
     def registro_profesor(self, nombre: str, numero: int) -> None:
+        """Registra datos del prfesor y hace uso de la clase Profesor
+
+        Args:
+            nombre (str): Almacena el nombre del profesor
+            numero (int): Almacena el numero de empleado del profesor
+        """
+        #Variable de la clase profesor
         self.__profesor= Profesor(nombre,numero)
     
     def registro_alumnos(self, alumno: Alumno = None, nombre: str = None, apellidop: str = None, apellidom: str = None ,nboleta: int = None,
                          dia: int = None, mes: int = None, año: int = None, carrera: str = None, grupo: str = None, correo: str = None) -> None:
+        """Se hara el registro de cada alumno ingresado por el usuario
+
+        Args:
+            alumno (Alumno, optional): Variable de tipo alumno. Defaults to None.
+        """
+        #Pregunta si la variable alumno no es del tipo None para agregarla a la variables _lista
         if alumno is not None:
             self.__lista.append(alumno)
         else:
+            #Si no es asi se crea la instancia
             alumno = Alumno(nombre, apellidop, apellidom, nboleta, dia, mes, año, carrera, grupo, correo)
             self.__lista.append(alumno)
         
     def impresion(self) -> None:
+        #Se manda a llamar las variables de la clase lista junto con la extencion de las clases correspondientes
         print(f'Profesor: {self.__profesor.nombre_profesor}')
         print(f'No. Empleado: {self.__profesor.n_empleado}')
         print('================================================================')
@@ -138,20 +164,27 @@ class Lista():
         print(f'Total de alumnos: {len(self.__lista)}.')
 
 def registros():
-    
+    """Guarda los datos registrados por el usuario
+    """
+    #Variable para uso de conteo  
     i=0
     while True:
         
         if i == 0:
+            #Registro del profesor
             print('\n\n********************')
             print('Datos del profesor')
+            
             nombre=input('Nombre Completo:')
             nempleado=int(input('Numero de empleado: '))
             lista_alumnos.registro_profesor(nombre, nempleado)
+            
             print('********************')
+            
             print('\n\n////////////////////////////////')
             print('\n Datos de Alumnos')
-            
+        
+        #Registro de Alumnos
         print(f'Alumnno {i+1}')
         apellidop=input('Apellido paterno:')
         apellidom=input('Apellido materno:')
@@ -166,6 +199,8 @@ def registros():
         print('\n')
         i+=1
         
+        '''La variable de tipo Lista accede al metodo registro_alumnos y manda a llamar a la variables
+        alumno que es de tipo Alumno para instanciar y agregarla a la lista lista_alumnos.'''
         lista_alumnos.registro_alumnos(alumno = Alumno(a_nombre,apellidop,apellidom,nboleta,dia,mes,año,carrera,grupo,correo))
         
         x=input('Desea agregar mas alumnos: Sí "1", No "0": ')
@@ -174,5 +209,6 @@ def registros():
         
 lista_alumnos= Lista()
 registros()
+#Realiza una limpieza de pantalla para solo ver los alumnos en el 'sistema'
 os.system('cls')
 lista_alumnos.impresion()
