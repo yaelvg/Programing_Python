b9cd97147c3c577907f85142a87b31933c0756f7
Alejandro Valdez <av192851@gmail.com>
2023-10-02 22:18:04 -0600

    Practica 3

--- Practicas/Practica3_1.py
+++ Practicas/Practica3_1.py
@@ -6,24 +6,29 @@ Practica 3
 Fecha: 4/10/2023
 '''
 from abc import ABCMeta, abstractmethod
+
 #Clase madre
 class Computadora(metaclass=ABCMeta):
+    """Clase abstracta Madre
+    """
     
     def __init__(self,memoria: str, procesador: str, almacenamiento: str, gpu: str) -> None:
-        """_summary_
+        """Constructor de clase Computadora
 
         Args:
-            memoria (str): ...
-            procesador (str): ...
-            almacenamiento (str): ...
-            gpu (str): ...
+            memoria (str):Tipo y cantidad de memoria  que tendra cada instancia de la subclase
+            procesador (str):Tipo procesador que tendra cada instancia de la subclase
+            almacenamiento (str):Tipo y cantidad de almacenamiento que tendra cada instancia de la subclase
+            gpu (str): Tipo de GPU que tendra cada instancia de la subclase
         """
+        
+        '''Se inicilizan los atributos y asi solo para las clases que adjunten nuevos atributos'''
         self.__memoria = memoria
         self.__procesador = procesador
         self.__almacenamiento = almacenamiento
         self.__gpu = gpu
         
-    #Getters y setters
+    #Getters y setters  (Encapsulamiento)
     @property
     def memoria(self):
         return self.__memoria
@@ -54,6 +59,9 @@ class Computadora(metaclass=ABCMeta):
     
     @abstractmethod
     def caracteristicas(self):
+        """Muestra las caracteristicas de cada intancia, las cuales con los atributos de la
+        clase madre abstracta. 
+        """
         print('\n\t\t********Factura********\n')
         print('\tComputadora Portatil:')
         print(f'Memoria RAM: {self.memoria} GB')
@@ -64,13 +72,25 @@ class Computadora(metaclass=ABCMeta):
         print(f'Número de puertos: {self.__n_puertos}')
     
 class ComputadoraPortatil (Computadora):
+    """Clase heredada con 2 atributos adicionales 'n_puertos' y 'pulgadas'
+
+    Args:
+        Computadora (class): Clase Madre
+    """
     
     def __init__(self,memoria: str, procesador: str, almacenamiento: str, gpu: str, pulgadas : int, n_puertos: str):
+        """Constructor de clase ComputadoraPortatil
+
+        Args:
+            pulgadas (int): numero de pulgadas de la instancia
+            n_puertos (str): numero de puertos de la instancia
+        """
+        '''Se inicializan los atributos de la clase superior "Computadora" '''
         Computadora.__init__(self, memoria, procesador, almacenamiento, gpu)
         self.__pulgadas = pulgadas
         self.__n_puertos = n_puertos
     
-    #Getters y setters
+    #Getters y setters  (Encapsulamiento)
     @property
     def pulgadas(self):
         return self.__pulgadas
@@ -87,6 +107,9 @@ class ComputadoraPortatil (Computadora):
             
     #Sobrecarga de metodos
     def caracteristicas(self):
+        """Muestra las caracteristicas de cada intancia, las cuales con los atributos de la
+        clase madre abstracta. 
+        """
         print('\n\t\t********Factura********\n')
         print('\tComputadora Portatil:')
         print(f'Memoria RAM: {self.memoria} GB')
@@ -97,17 +120,27 @@ class ComputadoraPortatil (Computadora):
         print(f'Número de puertos: {self.__n_puertos}')
 
 class ComputadoraEscritorio (Computadora):
-    """Clase ComputadoraEscritorio heradada 
+    """Clase heradada con 3 atributos adicionales 'peso', 'largo' y 'ancho'
 
     Args:
-        Computadora (class): Clase madre
+        Computadora (class): Clase Madre
     """
     def __init__(self, memoria: str, procesador: str, almacenamiento: str, gpu: str, peso: float, largo: float, ancho : float) -> None:
+        """Constructor de clase ComputadoraEscritorio
+
+        Args:
+            peso (float): Peso de la instancia
+            largo (float): largo de la instancia
+            ancho (float): ancho de la instancia
+        """
+        
+        '''Se inicializan los atributos de la clase superior "Computadora" '''
         Computadora.__init__(self,memoria, procesador, almacenamiento, gpu)
         self.__peso = peso
         self.__largo = largo
         self.__ancho = ancho    
-    #getters y setters
+        
+    #Getters y setters  (Encapsulamiento)
     @property
     def precio(self):
         return self.__peso
@@ -131,6 +164,9 @@ class ComputadoraEscritorio (Computadora):
         
     #Sobrecarga de metodos
     def caracteristicas(self):
+        """Muestra las caracteristicas de cada intancia, las cuales con los atributos de la
+        clase madre abstracta.
+        """
         print('\n\t\t********Factura********\n')
         print('\tComputadora de Escritorio:')
         print(f'Memoria RAM: {self.memoria} GB')
@@ -140,22 +176,29 @@ class ComputadoraEscritorio (Computadora):
         print(f'Peso: {self.__peso} kg')
         print(f'largo: {self.__largo}')    
         print(f'ancho: {self.__ancho}')    
-     
-        
-    
+          
 class TelefonoInteligente (Computadora):
-    """Clase TelefonoInteligente heredada
+    """Clase heredada con 2 atributos adicionales 'sistema_op', 'color', 'marca'.
 
     Args:
         Computadora (class): Clase madre
     """
     def __init__(self,memoria: str, procesador: str, almacenamiento: str, gpu: str, sistema_op: str , color: str, marca: str) -> None:
+        """Constructor de Clase TelefonoInteligente
+
+        Args:
+            sistema_op (str): Sistema operativo de la instacia
+            color (str): Color caracteristico de la instencia
+            marca (str): Marca de la instencia
+        """
+        
+        '''Se inicializan los atributos de la clase superior "Computadora" '''
         Computadora.__init__(self,memoria, procesador, almacenamiento, gpu)
         self.__sistema_op = sistema_op
         self.__color = color
         self.__marca = marca
         
-    #Getters y setters
+    #Getters y setters  (Encapsulamiento)
     @property
     def sistema_op(self):
         return self.__sistema_op
@@ -173,6 +216,9 @@ class TelefonoInteligente (Computadora):
     #Sobrecarga de metodos
 
     def caracteristicas(self):
+        """Muestra las caracteristicas de cada intancia, las cuales con los atributos de la
+        clase madre abstracta.
+        """
         print('\n\t\t********Factura********\n')
         print('\tTelefono Inteligente:')
         print(f'Memoria RAM: {self.memoria} GB')
@@ -182,18 +228,26 @@ class TelefonoInteligente (Computadora):
         print(f'Sistema Operativo: {self.__sistema_op}')
         print(f'Color: {self.__color}')    
 
-
 class Tablet (Computadora):
-    """Clase Tablet heredada
+    """Clase heredada con 2 atributos adicionales 'accesorios' y 'garantia'
 
     Args:
         Computadora (class): Clase madre
     """
     def __init__(self,memoria: str, procesador: str, almacenamiento: str, gpu: str, accesorios: list, garantia: str ):
+        """Constructor de la clase Tablet
+
+        Args:
+            accesorios (list): Accesorios que tendra la instancia
+            garantia (str): Garantia en años o meses de la instancia
+        """
+    
+        '''Se inicializan los atributos de la clase superior "Computadora" '''    
         Computadora.__init__(self, memoria, procesador, almacenamiento, gpu)
         self.__acesorios = accesorios
         self.__garantia = garantia
     
+    #Getters y setters  (Encapsulamiento)
     @property
     def accesorios(self):
         return self.__acesorios
@@ -210,6 +264,9 @@ class Tablet (Computadora):
     
     #Sobrecarga de metodos
     def caracteristicas(self):
+        """Muestra las caracteristicas de cada intancia, las cuales con los atributos de la
+        clase madre abstracta.
+        """
         print('\n\t\t********Factura********\n')
         print('\tComputadora de Escritorio:')
         print(f'Memoria RAM: {self.memoria} GB')
@@ -220,12 +277,12 @@ class Tablet (Computadora):
         print(f'Garantia: {self.__garantia}')   
         
 '''Principal'''
-#Instancias de tipo computadora portatil
+#Instancias de tipo computadora portatil incializadas
 lap_a=ComputadoraPortatil(' DDR4 8GB RAM',' Intel Core i5-11600K','Samsung 970 EVO Plus 1TB NVMe SSD','NVIDIA GeForce RTX 3080','13',5)
 lap_b=ComputadoraPortatil('Corsair Vengeance LPX 16GB DDR4 RAM','AMD Ryzen 7 5800X','Western Digital WD Black 4TB HDD','AMD Radeon RX 6800 XT','14',5)
 lap_c=ComputadoraPortatil('Kingston HyperX Fury 32GB DDR4 RAM','Western Digital WD Black 4TB HDD','Crucial MX500 500GB SATA SSD','Integrated Intel Iris Xe Graphics','15',7)
 
-#Instancias de computadora de Escritorio
+#Instancias de computadora de Escritorio incializadas
 compu_a=ComputadoraEscritorio('DDR4 8GB','AMD Ryzen 5 5600X','Kingston SNV2S/1000G 1TB','NVIDIA GeForce RTX 3080',1.1,20.9,10.7)
 compu_b=ComputadoraEscritorio('DDR4 16GB','Intel Core i7-10700K','Samsung 970 EVO NVMe SSD','Nvidia Evga SC RTX 20 Series RTX 2060 06G-P4-2062-KR 6GB',1.3,18.0,8.0)
 compu_c=ComputadoraEscritorio('DDR4 32GB','AMD Ryzen 3 3300X','Western Digital Purple Wd10purz 1TB','Nvidia PNY Quadro Series P620',1.6,19.5,11.2)
@@ -235,7 +292,7 @@ tel_a=TelefonoInteligente('8 RAM','xxx','32','xx','IOS','Rojo', 'Apple')
 tel_b=TelefonoInteligente('12 RAM','xxx','64','Adreno 660','Android','Negro','Xiomi')
 tel_c=TelefonoInteligente('12 RAM','xxx','128','Adreno 660','Android','Azul','Sony')
 
-#Instancias de tableta
+#Instancias de tablet incializadas
 tablet_a=Tablet('4 RAM','ARM','16','XX',['Teclado','Lapiz','Bocina'],'3 meses')
 tablet_b=Tablet('8 RAM','Intel ATOM','64','Adreno 640',['Funda', 'Lapiz','Audifonos'],'8meses')
 tablet_c=Tablet('8 RAM','Intel Core','128','Nvidia',['Lapiz','smartwatch'],'1 año')
--- Practicas/Practica3_2.py
+++ Practicas/Practica3_2.py
@@ -9,15 +9,25 @@ from abc import ABCMeta, abstractmethod
 
 #Clase madre
 class Mascota(metaclass=ABCMeta): 
-    """Clase Madre
+    """Clase abstracta madre
     """
     def __init__(self, nombre: str, edad: int, duenio: str, tipo: str) -> None:
+        """Constructor de la clase madre Mascota
+
+        Args:
+            nombre (str): Nombre de la mascota
+            edad (int): Edad de la mascota
+            duenio (str): Nombre del duenio
+            tipo (str): Clasificacion de la mascota
+        """
+        
+        '''Se inicilizan los atributos y asi solo para las clases que adjunten nuevos atributos'''
         self.__nombre = nombre
         self.__edad = edad
         self.__duenio = duenio
         self.__tipo = tipo
     
-    #getters y setters
+    #Getters y setters  (Encapsulamiento)
     @property
     def nombre(self):
         return self.__nombre
@@ -49,52 +59,75 @@ class Mascota(metaclass=ABCMeta):
     #Metodo habla
     @abstractmethod
     def habla(self):
+        """Mostrara cuidados y caracteristicas dependiendo la clase haciendo una sobrecarga(Polimorfismo)
+        """
         pass
     
     #Metodo tostring
     @abstractmethod
     def __str__(self) -> str:
+        """Mostrara en consola los atributos caracteristicos de cada clase
+
+        Returns:
+            str: Retorna solo datos de tipo str
+        """
         pass
 
 class Domestica(Mascota, metaclass= ABCMeta):
-    """Clase Domestica heradada
+    """Clase heradada con un atributo adicional 'ternura'
+        (ABSTRACTA)
 
     Args:
         Mascota (class): Clase madre
     """
+    
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, ternura: int):
+        """Constructor de la clase Domestica
+
+        Args:
+            ternura (int): numero de la ternura en escalra (1-10)
+        """
+        
+        '''Se inicializan los atributos de la clase superior "Mascota" '''        
         Mascota.__init__(self, nombre, edad, duenio, tipo)
         self.__ternura = ternura
         
-    #getters y setters
+    #Getters y setters  (Encapsulamiento)
     @property
     def ternura(self):
         return self.__ternura
     @ternura.setter
     def ternura(self,ternura):
         self.__ternura =ternura
-    
-    #toString
-    @abstractmethod
-    def __str__(self) -> str:
-        pass
-    
+        
     #Metodo habla
     @abstractmethod
     def habla(self):
         pass
 
-class Exotica (Mascota, metaclass=ABCMeta) :
-    """Clase Exotica heredada
+    #toString
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
 
+class Exotica (Mascota, metaclass=ABCMeta) :
+    """Clase heredada con un atributo adicional 'peligro'
+        (ABSTRACTA)
     Args:
         Mascota (class): Clase madre
     """
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, peligro: int):
+        """Constructor de la clase heredada
+
+        Args:
+            peligro (int): Numero de peligro en un rango (1-10)
+        """
+        
+        '''Se inicializan los atributos de la clase superior "Mascota" '''        
         Mascota.__init__(self, nombre, edad, duenio, tipo)
         self.__peligro = peligro
     
-    #getters y setters
+    #Getters y setters  (Encapsulamiento)
     @property
     def peligro(self):
         return self.__peligro
@@ -114,8 +147,15 @@ class Exotica (Mascota, metaclass=ABCMeta) :
 
 #clase submadre Domestica
 class Gato(Domestica):
+    """Clase heredada
+    
+    Args:
+        Domestica (class): Clase Domestica (submadre)
+    """
     
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, ternura: str) -> None:
+        
+        '''Se inicializa los atributos de la clase submadre (Domestica)'''
         Domestica.__init__(self, nombre, edad, duenio, tipo, ternura)
     
     def habla(self)-> str:
@@ -131,8 +171,15 @@ class Gato(Domestica):
         return f'Nombre: {self.nombre}\nEdad: {self.edad} meses\nDuenio: {self.duenio}\nTipo: {self.tipo}\nFactor de ternura: {self.ternura}'
 
 class Perro(Domestica):
+    """Clase heredada
+
+    Args:
+        Domestica (class): Clase Domestica (submadre)
+    """
     
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, ternura: str):
+        
+        '''Se inicializa los atributos de la clase submadre (Domestica)'''
         Domestica.__init__(self, nombre, edad, duenio, tipo, ternura)
     
     def habla(self)-> str:
@@ -148,10 +195,16 @@ class Perro(Domestica):
         return f'Nombre: {self.nombre}\nEdad: {self.edad} meses\nDuenio: {self.duenio}\nTipo: {self.tipo}\nFactor de ternura: {self.ternura}'
 
     
-#clase submadre Exotica
 class Vivora(Exotica):
+    """Clase heradada
+
+    Args:
+        Exotica (class): clase Exotica (submadre)
+    """
     
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, peligro: int):
+        
+        '''Se inicializa los atributos de la clase submadre (Exotica)'''
         Exotica.__init__(self, nombre, edad, duenio, tipo, peligro)
 
     def habla(self)-> str:
@@ -166,8 +219,15 @@ class Vivora(Exotica):
         return f'Nombre: {self.nombre}\nEdad: {self.edad} meses\nDuenio: {self.duenio}\nTipo: {self.tipo}\nFactor de peligro: {self.peligro}'
 
 class Tigre(Exotica):
+    """Clase heredada
+
+    Args:
+        Exotica (class): Clase Exotica (Submadre)
+    """
     
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, peligro: int)-> None:
+        
+        '''Se inicializa los atributos de la clase submadre (Exotica)'''
         Exotica.__init__(self, nombre, edad, duenio,tipo, peligro)
 
     def habla(self)-> None:
@@ -183,11 +243,20 @@ class Tigre(Exotica):
 
     
 class Dinosaurio(Exotica, metaclass=ABCMeta):
+    """Clase heredada (EXOTICA) con un atributo adicional 'tipo_dinosaurio
+        (ABSTRACTA)
+
+    Args:
+        Exotica (class): Clase Exotica (Submadre)
+    """
     
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, peligro: int, tipo_dinosaurio: str)-> None:
+        
+        '''Se inicializan los atributos de la clase Exotica'''
         Exotica.__init__(self, nombre, edad, duenio, tipo, peligro)
         self.__tipo_dinosaurio = tipo_dinosaurio
-    
+        
+    #Getters y setters  (Encapsulamiento)    
     @property
     def tipo_dinosaurio(self):
         return self.__tipo_dinosaurio
@@ -201,6 +270,11 @@ class Dinosaurio(Exotica, metaclass=ABCMeta):
 #Subclasese de dinosaurio
 
 class Brontosaurio(Dinosaurio):
+    """Clase heredada
+
+    Args:
+        Dinosaurio (class): _description_
+    """
     
     def __init__(self, nombre : str, edad: int, duenio: str, tipo: str, peligro: int, tipo_dinosaurio = 'Brontosaurio'):
         Dinosaurio.__init__(self, nombre, edad, duenio,tipo, peligro, tipo_dinosaurio)
