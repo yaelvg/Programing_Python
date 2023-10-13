9ec0ee991a24e5a979df60d47479e2cfa7e77e3f
Alejandro Valdez <av192851@gmail.com>
2023-10-12 20:09:35 -0600

    GUI

---
+++ GUI/VENTANA.py
@@ -0,0 +1,20 @@
+from Ui_VENTANA_1 import *
+from PyQt6.QtWidgets import QMainWindow, QMessageBox
+import sys
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *parent, **flags) -> None:
+        super().__init__(*parent, **flags)
+        self.setupUi(self)
+        #Conecta el boton
+        self.pushButton.clicked.connect(self.showMessage)
+        
+    def showMessage(self):
+        QMessageBox.information(self, 'Message', f'Hola {self.lineEdit.text()}')
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()    
+    window.show()
+    sys.exit(app.exec())
+        
